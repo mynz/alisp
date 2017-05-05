02d43c4df20e609837a9ef19ebd0aa85568b384b
mynz <rollingteapot@gmail.com>
2017-05-06 02:14:21 +0900

    write Next method

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,9 +1,12 @@
 package lexer
 
 import (
-	// "fmt"
+	"errors"
+	"fmt"
 	"io"
+	"strings"
 	"text/scanner"
+	"unicode"
 )
 
 type Lex struct {
@@ -21,6 +24,60 @@ func New(r io.Reader) *Lex {
 	return &Lex{
 		Scanner: &s,
 	}
+}
+
+func (l *Lex) Next() (string, error) {
+
+	// skip comments until EOF or newline.
+	for {
+		token := l.Peek()
+		if token != ';' {
+			break
+		}
+		l.Scan()
+		for {
+			next := l.Scanner.Next()
+			if next == scanner.EOF {
+				l.Token = scanner.EOF
+				return "", nil
+			} else if next == '\n' || next == '\r' {
+				break
+			}
+		}
+	}
+
+	l.Scan()
+	switch l.Token {
+	case '#':
+		switch peek := l.Peek(); peek {
+		case 't', 'f':
+			l.Scan()
+			return fmt.Sprintf("#%s", l.TokenText()), nil
+		default:
+			return "", errors.New("unknown hash symbol")
+		}
+
+	case '(', ')', '\'', scanner.EOF:
+		return l.TokenText(), nil
+	case '\n', '\r', '\t', ' ':
+		l.Scan()
+		return l.Next()
+	default:
+		token := l.TokenText()
+		for {
+			r := l.Peek()
+			if unicode.IsSpace(r) || strings.ContainsRune("()'.,;", r) || r == scanner.EOF {
+				break
+			}
+			token = fmt.Sprintf("%s%c", token, r)
+			l.Scanner.Next()
+		}
+		return token, nil
+	}
+}
+
+func (l *Lex) IsTokenString() bool {
+	return l.Token == scanner.String
 }
 
 func (lex *Lex) Scan() {
