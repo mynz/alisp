d2821377149195f93d7a0e10d308a19317bb473e
mynz <rollingteapot@gmail.com>
2017-05-07 00:54:58 +0900

    write parseList func in parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,11 @@
 package parser
 
 import (
+	"io"
+	"text/scanner"
+
 	"github.com/mynz/alisp/lexer"
+	"github.com/mynz/alisp/types"
 )
 
 type Parser struct {
@@ -10,4 +14,26 @@ type Parser struct {
 
 func New(lex *lexer.Lex) *Parser {
 	return &Parser{lex}
+}
+
+func (p *Parser) parseList() ([]types.Expression, error) {
+	var list []types.Expression
+
+	// recursive scan until ")"
+	for {
+		if p.lex.Peek() == scanner.EOF {
+			return nil, io.EOF
+		}
+		if p.lex.Peek() == ')' {
+			break
+		}
+		ex, err := p.Parse()
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, ex)
+	}
+	// detect by Peek(), so scanner should read next rune.
+	p.lex.Scan()
+	return list, nil
 }
