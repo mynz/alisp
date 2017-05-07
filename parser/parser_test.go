709fe9c0c8c113d290fdd2531d806efea8598a81
mynz <rollingteapot@gmail.com>
2017-05-07 16:18:59 +0900

    write more test code

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -13,7 +13,7 @@ func TestParser(t *testing.T) {
 	r := strings.NewReader("(print 1)")
 	parser := New(lexer.New(r))
 
-	actuals := []types.Expression{
+	expected := []types.Expression{
 		types.Symbol("print"),
 		types.Number(1),
 	}
@@ -21,8 +21,44 @@ func TestParser(t *testing.T) {
 	if err != nil {
 		t.Fatalf("paser failed: %s", err)
 	}
-	if !reflect.DeepEqual(exps, actuals) {
+	if !reflect.DeepEqual(exps, expected) {
 		t.Fatalf("expressions are not expected. %v", exps)
 	}
+}
 
+func TestParserRecursive(t *testing.T) {
+	r := strings.NewReader("(define (square x) (* x x))")
+	parser := New(lexer.New(r))
+	expected := []types.Expression{
+		types.Symbol("define"),
+		[]types.Expression{
+			types.Symbol("square"),
+			types.Symbol("x"),
+		},
+		[]types.Expression{
+			types.Symbol("*"),
+			types.Symbol("x"),
+			types.Symbol("x"),
+		},
+	}
+	actual, err := parser.Parse()
+	if err != nil {
+		t.Fatalf("parser failed: %s", err)
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expressions is not expected. %#v", actual)
+	}
+}
+
+func TestParserQuote(t *testing.T) {
+	r := strings.NewReader("'(1 2)")
+	parser := New(lexer.New(r))
+	expected := types.NewList(types.Number(1), types.Number(2))
+	actual, err := parser.Parse()
+	if err != nil {
+		t.Fatalf("parser failed: %s", err)
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expression is not expected. %v", actual)
+	}
 }
