3c81025d220a8a02a4f9a3dd9c37c23f84888d8a
mynz <rollingteapot@gmail.com>
2017-07-08 00:07:29 +0900

    add arithmatic functions

--- eval/env.go
+++ eval/env.go
@@ -57,3 +57,56 @@ func Cdr(args ...types.Expression) (types.Expression, error) {
 func Cons(args ...types.Expression) (types.Expression, error) {
 	return &types.Pair{Car: args[0], Cdr: args[1]}, nil
 }
+
+func Print(args ...types.Expression) (types.Expression, error) {
+	if len(args) == 1 {
+		fmt.Println(args[0])
+	} else {
+		fmt.Println(args)
+	}
+	return nil, nil
+}
+
+func Add(args ...types.Expression) (types.Expression, error) {
+	sum, ok := args[0].(types.Number)
+	if !ok {
+		return nil, fmt.Errorf("given args is not number: %#v", args[0])
+	}
+	for _, adder := range args[1:] {
+		sum = sum + adder.(types.Number)
+	}
+	return sum, nil
+}
+
+func Subtract(args ...types.Expression) (types.Expression, error) {
+	sub, ok := args[0].(types.Number)
+	if !ok {
+		return nil, fmt.Errorf("given args is not number: %#v", args[0])
+	}
+	for _, s := range args[1:] {
+		sub = sub - s.(types.Number)
+	}
+	return sub, nil
+}
+
+func Multiply(args ...types.Expression) (types.Expression, error) {
+	mul, ok := args[0].(types.Number)
+	if !ok {
+		return nil, fmt.Errorf("given args is not number: %#v", args[0])
+	}
+	for _, m := range args[1:] {
+		mul = mul * m.(types.Number)
+	}
+	return mul, nil
+}
+
+func Divide(args ...types.Expression) (types.Expression, error) {
+	div, ok := args[0].(types.Number)
+	if !ok {
+		return nil, fmt.Errorf("given args is not number: %#v", args[0])
+	}
+	for _, d := range args[1:] {
+		div = div / d.(types.Number)
+	}
+	return div, nil
+}
